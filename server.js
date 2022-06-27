6ed9ac2e06d0f3f9cc66a3b655a55404fc5f0aef
Safwan Islam <saislam@davidson.edu>
2022-06-26 19:11:33 -0500

    initial set up, cycles through questions, tried to set up schema.sql

---
+++ server.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('console.table');
+
+const db = mysql.createConnection(
+  {
+    user: 'root',
+    database: 'classlist_db',
+  }
+);
+
+const fn = (options) => {
+  if (options === 'exit') return process.exit();
+
+  const query = 'SELECT * FROM students' + (('enrolled' in options) ? ' WHERE ?' : '');
+
+  db.query(query, options, function (err, results) {
+    if (err) return console.error(err);
+    console.table(results);
+    return init();
+  });
+};
+
+const init = () => {
+  const choices = [
+    { name: 'View All Employees', value: {} },
+    { name: 'Add Employee', value: {}, },
+    { name: 'Update Employee Role', value: {} },
+    { name: 'View All Roles', value: {} },
+    { name: 'Add Role', value: {} },
+    { name: 'View All Departments', value: {} },
+    { name: 'Add Departments', value: {} },
+    { name: 'Exit', value: 'exit' },
+  ];
+
+  inquirer.prompt([
+    {
+      type: 'rawlist',
+      name: 'query',
+      message: 'What what you like to do?',
+      choices,
+    }
+  ]).then((answers) => fn(answers.query));
+};
+
+init();
