e7fd03618c0f4610aff7b46b7a06da92b6a8a799
Safwan Islam <saislam@davidson.edu>
2022-06-28 14:39:22 -0500

    worked on the arrays of choices for users in order to select through a cycle of choices

--- server.js
+++ server.js
@@ -39,60 +39,83 @@ const allDepartments = () => {
 }
 
 const addEmployee = () => {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'employeeFirstName',
-      message: 'What is the first name of the employee you would like to add?'
-    },
-    {
-      type: 'input',
-      name: 'employeeLastName',
-      message: 'What is the last name of the employee?'
-    },
-    {
-      type: 'list',
-      name: 'employeeRoleId',
-      message: 'What is the role of the employee?'
-      // choices: array of employees goes here
-    },
-    {
-      type: 'list',
-      name: 'employeeManagerId',
-      message: 'Who is the manager of the employee?'
-      // choices: array of Managers goes here + a none option
-    }
-  ]).then(function (answer) {
-    db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)", [answer.employeeFirstName, answer.employeeLastName, answer.employeeRoleId, answer.employeeManagerId], function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      init();
+  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id', function (err, res) {
+    if (err) return console.error(err);
+    const managerChoices = res.map(({ id, manager }) => ({
+      name: manager,
+      value: id
+    })).filter(e => e);
+    db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id=department.id', function (err, results) {
+      if (err) return console.error(err);
+      const roleChoices = results.map(({ id, title }) => ({
+        name: title,
+        value: id
+      }));
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'employeeFirstName',
+          message: 'What is the first name of the employee you would like to add?'
+        },
+        {
+          type: 'input',
+          name: 'employeeLastName',
+          message: 'What is the last name of the employee?'
+        },
+        {
+          type: 'list',
+          name: 'employeeRoleId',
+          message: 'What is the role of the employee?',
+          choices: roleChoices
+        },
+        {
+          type: 'list',
+          name: 'employeeManagerId',
+          message: 'Who is the manager of the employee?',
+          choices: managerChoices
+        }
+      ]).then(function (answer) {
+        db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)", [answer.employeeFirstName, answer.employeeLastName, answer.employeeRoleId, answer.employeeManagerId], function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          init();
+        });
+      });
     });
+
   });
 }
 const addRole = () => {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'roleName',
-      message: 'What is the name of role you would like to add?'
-    },
-    {
-      type: 'list',
-      name: 'roleDeptId',
-      message: 'What is the department name?'
-      // choices: array of departments goes here
-    },
-    {
-      type: 'input',
-      name: 'roleSalary',
-      message: 'What is the salary of the role?'
-    }
-  ]).then(function (answer) {
-    db.query("INSERT INTO role (title, department_id, salary) VALUES (?,?,?)", [answer.roleName, answer.roleDeptId, answer.roleSalary], function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      init();
+  db.query('SELECT * FROM department', function (err, results) {
+    if (err) return console.error(err);
+    console.log(results);
+    const deptChoices = results.map(({ id, name }) => ({
+      name: name,
+      value: id
+    }));
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'roleName',
+        message: 'What is the name of role you would like to add?'
+      },
+      {
+        type: 'list',
+        name: 'roleDeptId',
+        message: 'What is the department name?',
+        choices: deptChoices
+      },
+      {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'What is the salary of the role?'
+      }
+    ]).then(function (answer) {
+      db.query("INSERT INTO role (title, department_id, salary) VALUES (?,?,?)", [answer.roleName, answer.roleDeptId, answer.roleSalary], function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+      });
     });
   });
 }
@@ -112,22 +135,36 @@ const addDepartment = () => {
 }
 
 const updateRole = () => {
-  inquirer.prompt({
-    type: 'list',
-    name: 'updateEmployee',
-    message: 'Who would you like to update?'
-    // choices: array of employees goes here
-  },
-  {
-    type: 'list',
-    name: 'updateRole',
-    message: 'What would you like to update their role to?'
-    // choices: array of roles goes here
-  }).then(function (answer) {
-    db.query('UPDATE employee SET role_id=? WHERE id=?', [answer.updateRole, answer.updateEmployee], function (err, res) {
-      if (err) throw err;
-      console.table(res)
-      init();
+  db.query('SELECT employee.id, CONCAT(employee.first_name, employee.last_name) AS employee, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id', function (err, res) {
+    if (err) return console.error(err);
+    const employeeChoices = res.map(({ id, employee }) => ({
+      name: employee,
+      value: id
+    })).filter(e => e);
+    db.query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id=department.id', function (err, results) {
+      if (err) return console.error(err);
+      const roleChoices = results.map(({ id, title }) => ({
+        name: title,
+        value: id
+      }));
+      inquirer.prompt({
+        type: 'list',
+        name: 'updateEmployee',
+        message: 'Who would you like to update?',
+        choices: employeeChoices
+      },
+        {
+          type: 'list',
+          name: 'updateRole',
+          message: 'What would you like to update their role to?',
+          choices: roleChoices
+        }).then(function (answer) {
+          db.query('UPDATE employee SET role_id=? WHERE id=?', [answer.updateRole, answer.updateEmployee], function (err, res) {
+            if (err) throw err;
+            console.table(res)
+            init();
+          });
+        });
     });
   });
 }
